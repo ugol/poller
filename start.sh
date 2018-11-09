f050c32ef2796278258a43bf14ec647ae2d12daa
Spagno <spagno@redhat.com>
2018-11-09 17:18:12 +0100

    fixed start.sh

--- start.sh
+++ start.sh
@@ -4,6 +4,7 @@ if [ "${typology}" == "results" ]
 then
   execute="results"
 elif [ "${typology}" == "poller" ]
+then
   execute="start --pollerAddress=$HOSTNAME:9090"
 fi
 export APP_ID && /go/bin/app $execute
