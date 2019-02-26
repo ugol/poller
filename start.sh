e4df394f0729817f82201aa0a218ce4f76cf2ed9
spagno <spagno@redhat.com>
2019-02-26 22:45:48 +0100

    default is poller

--- start.sh
+++ start.sh
@@ -3,9 +3,7 @@ APP_ID=${HOSTNAME:(-5)}
 if [ "${typology}" == "results" ]
 then
   execute="results --resultAddress=$HOSTNAME:9090"
-elif [ "${typology}" == "poller" ]
-then
+else
   execute="start --pollerAddress=$HOSTNAME:9090"
 fi
-#export APP_ID && /go/bin/poller $execute
 export APP_ID && ./poller $execute
