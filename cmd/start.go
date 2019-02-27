e4eb8f4aab56f50f2eac3e137f004df16f4b5948
Spagno <spagno@redhat.com>
2019-02-27 04:32:41 +0100

    added return page to already voted

--- cmd/start.go
+++ cmd/start.go
@@ -82,6 +82,7 @@ func PollHandler(w http.ResponseWriter, r *http.Request) {
 
 		if hasVoted != nil && hasVoted.Value == poll {
 			fmt.Fprint(w, "You have already voted for this poll\n")
+                        fmt.Fprintf(w, "<br><a href=\"../../../static/results.html?poll=%v\">Go to results.</a>", poll)
 			log.Print("You have already voted for this poll\n")
 		} else {
 			if score.VoteFor(poll, vote) {
@@ -91,7 +92,7 @@ func PollHandler(w http.ResponseWriter, r *http.Request) {
 				w.Header().Set("Content-Type", "text/html; charset=UTF-8")
 				w.WriteHeader(http.StatusOK)
 				fmt.Fprintf(w, "You voted: %v\n", vote)
-                                fmt.Fprintf(w, "<a href=\"../../../static/results.html?poll=%v\">Go to results.</a>", poll)
+                                fmt.Fprintf(w, "<br><a href=\"../../../static/results.html?poll=%v\">Go to results.</a>", poll)
 				log.Printf("Vote received: %v\n", vote)
 
 			} else {
